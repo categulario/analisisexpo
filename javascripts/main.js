a24ca447807c9e36b6ecf3a6b5ce43d935d2fa42
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2014-10-26 09:30:14 -0600

    embed pdf with pdfjs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+console.log('starting');
+
+PDFJS.getDocument('presentacion.pdf').then(function (pdf) {
+    console.log('we have the document');
+
+    pdf.getPage(1).then(function (page) {
+        console.log('we have the first page');
+
+        var scale     = 1.5;
+        var viewport  = page.getViewport(scale);
+
+        var canvas    = document.getElementById('presentacion');
+        var context   = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width  = viewport.width;
+
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        page.render(renderContext);
+    });
+});
