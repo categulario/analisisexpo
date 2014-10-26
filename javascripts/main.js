7f412547d8235d6781756a5a233d05667561bdd6
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2014-10-26 10:00:31 -0600

    the PDF viewer

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,22 +2,58 @@ console.log('starting');
 
 PDFJS.getDocument('presentacion.pdf').then(function (pdf) {
     console.log('we have the document');
+    window.PDF = pdf;
 
-    pdf.getPage(1).then(function (page) {
-        console.log('we have the first page');
+    $('#num-pages').text(pdf.numPages);
 
-        var scale     = 1.5;
-        var viewport  = page.getViewport(scale);
+    pdf.getPage(1).then(renderPage);
+});
 
-        var canvas    = document.getElementById('presentacion');
-        var context   = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width  = viewport.width;
+$(function () {
+    console.log('document loaded');
 
-        var renderContext = {
-          canvasContext: context,
-          viewport: viewport
-        };
-        page.render(renderContext);
+    $('#back').click(function () {
+        console.log('go back');
+
+        var currentPage = parseInt($('#page').text());
+
+        if (currentPage != 1) {
+            currentPage--;
+            PDF.getPage(currentPage).then(renderPage);
+            $('#page').text(currentPage);
+        }
+
+        return false;
+    });
+    $('#forw').click(function () {
+        console.log('go forward');
+
+        var currentPage = parseInt($('#page').text());
+
+        if (currentPage < PDF.numPages) {
+            currentPage++;
+            PDF.getPage(currentPage).then(renderPage);
+            $('#page').text(currentPage);
+        }
+
+        return false;
     });
 });
+
+function renderPage(page) {
+    console.log('we have a page');
+
+    var scale     = 1.5;
+    var viewport  = page.getViewport(scale);
+
+    var canvas    = document.getElementById('presentacion');
+    var context   = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width  = viewport.width;
+
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    page.render(renderContext);
+}
